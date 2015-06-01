157dbd07c907cc5785dccc9272d8dc663233f809
Rodrigo Vilar <rvilar@gmail.com>
2015-06-01 20:30:44 -0300

    A_5

--- src/main/java/br/edu/ufcg/embedded/roocodeduplication/ProductController.java
+++ src/main/java/br/edu/ufcg/embedded/roocodeduplication/ProductController.java
@@ -1,5 +1,5 @@
 package br.edu.ufcg.embedded.roocodeduplication;
-import br.edu.ufcg.embedded.roocodeduplication.domain.Developer;
+import br.edu.ufcg.embedded.roocodeduplication.domain.Product;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -22,180 +22,179 @@ import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 
-@RooWebJson(jsonObject = Developer.class)
+@RooWebJson(jsonObject = Product.class)
 @Controller
-@RequestMapping("/developers")
-@RooWebScaffold(path = "developers", formBackingObject = Developer.class)
-public class DeveloperController {
+@RequestMapping("/products")
+@RooWebScaffold(path = "products", formBackingObject = Product.class)
+public class ProductController {
 
-    @RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid Developer developer, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
+    public String create(@Valid Product product, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, developer);
-            return "developers/create";
+            populateEditForm(uiModel, product);
+            return "products/create";
         }
         uiModel.asMap().clear();
-        developer.persist();
-        return "redirect:/developers/" + encodeUrlPathSegment(developer.getId().toString(), httpServletRequest);
+        product.persist();
+        return "redirect:/products/" + encodeUrlPathSegment(product.getId().toString(), httpServletRequest);
     }
 
-    @RequestMapping(params = "form", produces = "text/html")
+	@RequestMapping(params = "form", produces = "text/html")
     public String createForm(Model uiModel) {
-        populateEditForm(uiModel, new Developer());
-        return "developers/create";
+        populateEditForm(uiModel, new Product());
+        return "products/create";
     }
 
-    @RequestMapping(value = "/{id}", produces = "text/html")
+	@RequestMapping(value = "/{id}", produces = "text/html")
     public String show(@PathVariable("id") Long id, Model uiModel) {
-        uiModel.addAttribute("developer", Developer.findDeveloper(id));
+        uiModel.addAttribute("product", Product.findProduct(id));
         uiModel.addAttribute("itemId", id);
-        return "developers/show";
+        return "products/show";
     }
 
-    @RequestMapping(produces = "text/html")
+	@RequestMapping(produces = "text/html")
     public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("developers", Developer.findDeveloperEntries(firstResult, sizeNo, sortFieldName, sortOrder));
-            float nrOfPages = (float) Developer.countDevelopers() / sizeNo;
+            uiModel.addAttribute("products", Product.findProductEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+            float nrOfPages = (float) Product.countProducts() / sizeNo;
             uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
         } else {
-            uiModel.addAttribute("developers", Developer.findAllDevelopers(sortFieldName, sortOrder));
+            uiModel.addAttribute("products", Product.findAllProducts(sortFieldName, sortOrder));
         }
-        return "developers/list";
+        return "products/list";
     }
 
-    @RequestMapping(method = RequestMethod.PUT, produces = "text/html")
-    public String update(@Valid Developer developer, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
+    public String update(@Valid Product product, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, developer);
-            return "developers/update";
+            populateEditForm(uiModel, product);
+            return "products/update";
         }
         uiModel.asMap().clear();
-        developer.merge();
-        return "redirect:/developers/" + encodeUrlPathSegment(developer.getId().toString(), httpServletRequest);
+        product.merge();
+        return "redirect:/products/" + encodeUrlPathSegment(product.getId().toString(), httpServletRequest);
     }
 
-    @RequestMapping(value = "/{id}", params = "form", produces = "text/html")
+	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
     public String updateForm(@PathVariable("id") Long id, Model uiModel) {
-        populateEditForm(uiModel, Developer.findDeveloper(id));
-        return "developers/update";
+        populateEditForm(uiModel, Product.findProduct(id));
+        return "products/update";
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
     public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
-        Developer developer = Developer.findDeveloper(id);
-        developer.remove();
+        Product product = Product.findProduct(id);
+        product.remove();
         uiModel.asMap().clear();
         uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
         uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
-        return "redirect:/developers";
+        return "redirect:/products";
     }
 
-    void populateEditForm(Model uiModel, Developer developer) {
-        uiModel.addAttribute("developer", developer);
+	void populateEditForm(Model uiModel, Product product) {
+        uiModel.addAttribute("product", product);
     }
 
-    String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
+	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
         String enc = httpServletRequest.getCharacterEncoding();
         if (enc == null) {
             enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
         }
         try {
             pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {
-        }
+        } catch (UnsupportedEncodingException uee) {}
         return pathSegment;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
     @ResponseBody
     public ResponseEntity<String> showJson(@PathVariable("id") Long id) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
         try {
-            Developer developer = Developer.findDeveloper(id);
-            if (developer == null) {
+            Product product = Product.findProduct(id);
+            if (product == null) {
                 return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
             }
-            return new ResponseEntity<String>(developer.toJson(), headers, HttpStatus.OK);
+            return new ResponseEntity<String>(product.toJson(), headers, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @RequestMapping(headers = "Accept=application/json")
+	@RequestMapping(headers = "Accept=application/json")
     @ResponseBody
     public ResponseEntity<String> listJson() {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json; charset=utf-8");
         try {
-            List<Developer> result = Developer.findAllDevelopers();
-            return new ResponseEntity<String>(Developer.toJsonArray(result), headers, HttpStatus.OK);
+            List<Product> result = Product.findAllProducts();
+            return new ResponseEntity<String>(Product.toJsonArray(result), headers, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @RequestMapping(method = RequestMethod.POST, headers = "Accept=application/json")
+	@RequestMapping(method = RequestMethod.POST, headers = "Accept=application/json")
     public ResponseEntity<String> createFromJson(@RequestBody String json, UriComponentsBuilder uriBuilder) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         try {
-            Developer developer = Developer.fromJsonToDeveloper(json);
-            developer.persist();
+            Product product = Product.fromJsonToProduct(json);
+            product.persist();
             RequestMapping a = (RequestMapping) getClass().getAnnotation(RequestMapping.class);
-            headers.add("Location", uriBuilder.path(a.value()[0] + "/" + developer.getId().toString()).build().toUriString());
+            headers.add("Location",uriBuilder.path(a.value()[0]+"/"+product.getId().toString()).build().toUriString());
             return new ResponseEntity<String>(headers, HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @RequestMapping(value = "/jsonArray", method = RequestMethod.POST, headers = "Accept=application/json")
+	@RequestMapping(value = "/jsonArray", method = RequestMethod.POST, headers = "Accept=application/json")
     public ResponseEntity<String> createFromJsonArray(@RequestBody String json) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         try {
-            for (Developer developer : Developer.fromJsonArrayToDevelopers(json)) {
-                developer.persist();
+            for (Product product: Product.fromJsonArrayToProducts(json)) {
+                product.persist();
             }
             return new ResponseEntity<String>(headers, HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
     public ResponseEntity<String> updateFromJson(@RequestBody String json, @PathVariable("id") Long id) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         try {
-            Developer developer = Developer.fromJsonToDeveloper(json);
-            developer.setId(id);
-            if (developer.merge() == null) {
+            Product product = Product.fromJsonToProduct(json);
+            product.setId(id);
+            if (product.merge() == null) {
                 return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
             }
             return new ResponseEntity<String>(headers, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
     public ResponseEntity<String> deleteFromJson(@PathVariable("id") Long id) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         try {
-            Developer developer = Developer.findDeveloper(id);
-            if (developer == null) {
+            Product product = Product.findProduct(id);
+            if (product == null) {
                 return new ResponseEntity<String>(headers, HttpStatus.NOT_FOUND);
             }
-            developer.remove();
+            product.remove();
             return new ResponseEntity<String>(headers, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<String>("{\"ERROR\":" + e.getMessage() + "\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
---
+++ src/main/java/br/edu/ufcg/embedded/roocodeduplication/domain/Product.java
@@ -0,0 +1,176 @@
+package br.edu.ufcg.embedded.roocodeduplication.domain;
+import flexjson.JSONDeserializer;
+import flexjson.JSONSerializer;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Version;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.roo.addon.json.RooJson;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configurable
+@Entity
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+@RooJson(deepSerialize = true)
+public class Product {
+
+	public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+	@PersistenceContext
+    transient EntityManager entityManager;
+
+	public static final List<String> fieldNames4OrderClauseFilter = java.util.Arrays.asList("");
+
+	public static final EntityManager entityManager() {
+        EntityManager em = new Product().entityManager;
+        if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
+        return em;
+    }
+
+	public static long countProducts() {
+        return entityManager().createQuery("SELECT COUNT(o) FROM Product o", Long.class).getSingleResult();
+    }
+
+	public static List<Product> findAllProducts() {
+        return entityManager().createQuery("SELECT o FROM Product o", Product.class).getResultList();
+    }
+
+	public static List<Product> findAllProducts(String sortFieldName, String sortOrder) {
+        String jpaQuery = "SELECT o FROM Product o";
+        if (fieldNames4OrderClauseFilter.contains(sortFieldName)) {
+            jpaQuery = jpaQuery + " ORDER BY " + sortFieldName;
+            if ("ASC".equalsIgnoreCase(sortOrder) || "DESC".equalsIgnoreCase(sortOrder)) {
+                jpaQuery = jpaQuery + " " + sortOrder;
+            }
+        }
+        return entityManager().createQuery(jpaQuery, Product.class).getResultList();
+    }
+
+	public static Product findProduct(Long id) {
+        if (id == null) return null;
+        return entityManager().find(Product.class, id);
+    }
+
+	public static List<Product> findProductEntries(int firstResult, int maxResults) {
+        return entityManager().createQuery("SELECT o FROM Product o", Product.class).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
+
+	public static List<Product> findProductEntries(int firstResult, int maxResults, String sortFieldName, String sortOrder) {
+        String jpaQuery = "SELECT o FROM Product o";
+        if (fieldNames4OrderClauseFilter.contains(sortFieldName)) {
+            jpaQuery = jpaQuery + " ORDER BY " + sortFieldName;
+            if ("ASC".equalsIgnoreCase(sortOrder) || "DESC".equalsIgnoreCase(sortOrder)) {
+                jpaQuery = jpaQuery + " " + sortOrder;
+            }
+        }
+        return entityManager().createQuery(jpaQuery, Product.class).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
+    }
+
+	@Transactional
+    public void persist() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.persist(this);
+    }
+
+	@Transactional
+    public void remove() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        if (this.entityManager.contains(this)) {
+            this.entityManager.remove(this);
+        } else {
+            Product attached = Product.findProduct(this.id);
+            this.entityManager.remove(attached);
+        }
+    }
+
+	@Transactional
+    public void flush() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.flush();
+    }
+
+	@Transactional
+    public void clear() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.clear();
+    }
+
+	@Transactional
+    public Product merge() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        Product merged = this.entityManager.merge(this);
+        this.entityManager.flush();
+        return merged;
+    }
+
+	public String toJson() {
+        return new JSONSerializer()
+        .exclude("*.class").deepSerialize(this);
+    }
+
+	public String toJson(String[] fields) {
+        return new JSONSerializer()
+        .include(fields).exclude("*.class").deepSerialize(this);
+    }
+
+	public static Product fromJsonToProduct(String json) {
+        return new JSONDeserializer<Product>()
+        .use(null, Product.class).deserialize(json);
+    }
+
+	public static String toJsonArray(Collection<Product> collection) {
+        return new JSONSerializer()
+        .exclude("*.class").deepSerialize(collection);
+    }
+
+	public static String toJsonArray(Collection<Product> collection, String[] fields) {
+        return new JSONSerializer()
+        .include(fields).exclude("*.class").deepSerialize(collection);
+    }
+
+	public static Collection<Product> fromJsonArrayToProducts(String json) {
+        return new JSONDeserializer<List<Product>>()
+        .use("values", Product.class).deserialize(json);
+    }
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+	@Version
+    @Column(name = "version")
+    private Integer version;
+
+	public Long getId() {
+        return this.id;
+    }
+
+	public void setId(Long id) {
+        this.id = id;
+    }
+
+	public Integer getVersion() {
+        return this.version;
+    }
+
+	public void setVersion(Integer version) {
+        this.version = version;
+    }
+}
